100eb4b448523ba7dcc578514938b5da6b35a9fc
Silvia Pan <silvia.pan29@gmail.com>
2024-09-05 20:31:26 -0400

    add solution for move-zeroes problem

---
+++ javascript/0282-Move-Zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  if (nums.length === 1) return nums;
+
+  let pointer = 0;
+  let zeroPointer = null;
+
+  while (pointer < nums.length) {
+    if (nums[pointer] !== 0 && zeroPointer !== null) {
+      nums[zeroPointer] = nums[pointer];
+      nums[pointer] = 0;
+      zeroPointer++;
+    }
+    if (nums[pointer] === 0 && zeroPointer === null) {
+      zeroPointer = pointer;
+    }
+
+    pointer++;
+  }
+};
